e33ba7d9eb55807a9ffe240da23824c66bbe167e
whoisfeb <whoisfeb19@gmail.com>
2026-06-17 12:19:43 +0800

    update welcome bot

--- handlers/welcomer.js
+++ handlers/welcomer.js
@@ -43,7 +43,8 @@ async function setupWelcomerHandler(client) {
             .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
             .setDescription(
                 `Hey, Welcome <@${member.id}>\n\n` +
-                `・ Silahkan ambil role anda di <#1392382456589717559>\n` +
+                `・ Silahkan ambil role anda di <#1392382456589717559>\n\n`
+                `・ Jika <#1392382456589717559> tidak muncul silahkan klik <#1514831379941294190>\n\n` +
                 `・ Buat akun/UCP di channel <#1392382456589717561>\n\n` +
                 `**Member ke:** #${totalMember}`
             )
