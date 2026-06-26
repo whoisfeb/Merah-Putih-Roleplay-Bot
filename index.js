876a944c5e80a11dc6f9614b985f5e48a4b101fd
whoisfeb <whoisfeb19@gmail.com>
2026-06-26 16:43:00 +0800

    test

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ const { setupLogsHandler } = require('./handlers/logs-discord');
 const { setupWelcomerHandler } = require('./handlers/welcomer');
 const { setupCommandsHandler } = require('./handlers/commands');
 const { setupAutoKarantinaHandler } = require('./handlers/auto-karantina');
+const { handleSendMessage } = require('./handlers/control.js');
 
 
 // --- CONFIG ---
@@ -213,6 +214,19 @@ const commands = [
             { name: 'user', type: 6, description: 'Penerima (user)', required: true },
             { name: 'code', type: 3, description: 'Kode topup (contoh: MPRP-65A5-U8ZG)', required: true },
             { name: 'note', type: 3, description: 'Catatan/validitas (opsional)', required: false },
+        ],
+    },
+    {
+        name: 'send-message',
+        description: ':crown: [OWNER ONLY] Mengirim pesan teks, gambar, atau file ke channel atau user tertentu',
+        options: [
+            // Konten Pesan
+            { name: 'teks', type: 3, description: 'Tulis isi teks pesan yang ingin dikirim', required: false },
+            { name: 'file', type: 11, description: 'Unggah gambar, video, atau dokumen file', required: false },
+            
+            // Target Tujuan (Opsional di form, namun wajib diisi salah satu saat dijalankan)
+            { name: 'channel', type: 7, description: 'Pilih text channel target tujuan kirim', channel_types: [0, 5], required: false },
+            { name: 'user', type: 6, description: 'Pilih akun user target tujuan kirim via DM', required: false },
         ],
     }
 ];
