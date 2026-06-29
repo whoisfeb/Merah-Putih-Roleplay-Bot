9093b17688f3aa14f0fff4b3c553d36b8d2ec61b
whoisfeb <whoisfeb19@gmail.com>
2026-06-29 08:27:58 +0800

    add admin role pada anti link

--- handlers/anti-link.js
+++ handlers/anti-link.js
@@ -7,7 +7,7 @@ const {
     MessageFlags 
 } = require('discord.js');
 
-const ADMIN_ROLE_IDS = ['112233'];
+const ADMIN_ROLE_IDS = ['1392382455981412398', '1392382455981412393', '1392382455981412397', '1392382455947989066'];
 const BAD_LINKS = ["free-nitro", "discord-gift", "steam-promo", "bit.ly/badlink", "https://discord.gg", "https://discord.com", "discord.gg", "cherry-girls"];
 
 const imageCache = new Map(); 
