ec2d4147a2a77ca7d77a2b0c20d9ae8b19c15cd5
whoisfeb <whoisfeb19@gmail.com>
2026-06-30 09:41:57 +0800

    hahha

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const unbanHandler = require('./handlers/tiket-unban');
 const paymentHandler = require('./handlers/payment');
 const reportStaffHandler = require('./handlers/report-staff');
 // Tambahkan ini di bagian bawah daftar require handler kamu
-const timeReactionRoleHandler = require('./handlers/timereactionrole.js');
+const timeReactionRoleHandler = require('./handlers/time-reaction-role.js');
 
 const { setupLogsHandler } = require('./handlers/logs-discord');
 const { setupWelcomerHandler } = require('./handlers/welcomer');
